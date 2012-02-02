7f3a56915bd2f54b0e25cf37f6ff076b54eb7420
Paul Romano <romano7@gmail.com>
2012-02-02 13:20:57 -0500

    Changed plot_flux to work with any number of bins.

--- cullen-pincell/openmc/plot_flux.py
+++ cullen-pincell/openmc/plot_flux.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-# Determine equal-lethargy width energies
-Emin = 1e-11
-Emax = 20.0
-u = math.log(Emax/Emin)/200.0
-E = [Emin*math.exp(i*u) for i in range(200)]
-
 # Read tally and uncertainty values
 flux = []
 unc = []
@@ -19,6 +13,13 @@ for line in open('tallies.out','r'):
         if words[-2] == '+/-':
             flux.append(float(words[-3]))
             unc.append(2*float(words[-1]))
+
+# Determine equal-lethargy width energies
+n_bins = len(flux)
+Emin = 1e-11
+Emax = 20.0
+u = math.log(Emax/Emin)/n_bins
+E = [Emin*math.exp(i*u) for i in range(n_bins)]
 
 # Make log-log plot with error bars
 plt.errorbar(E,flux,yerr=unc, fmt='k.')
