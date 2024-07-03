5bc640189a7d99419d9b9128aa0bfa869f322487
Paul Romano <paul.k.romano@gmail.com>
2024-07-03 08:40:21 -0500

    Fix material consistency for pmf022

--- icsbep/pu-met-fast-022/openmc/case-2/generate_materials.py
+++ icsbep/pu-met-fast-022/openmc/case-2/generate_materials.py
@@ -1,25 +1,12 @@
 import openmc
 
-mats = openmc.Materials()
+pu = openmc.Material(name = "98% Pu-239 in delta phase")
+pu.add_nuclide('Pu239', 3.6623e-02)
+pu.add_nuclide('Pu240', 6.6951e-04)
+pu.add_element('Ga', 2.1979e-03)
+pu.add_element('Fe', 1.4247e-04)
+pu.add_element('C', 2.9311e-04)
+pu.add_element('Ni', 1.8624e-03)
 
-mat = openmc.Material(1)
-mat.name = "98% Pu-239 in delta phase"
-mat.set_density('atom/b-cm', 0.04178839)
-mat.add_nuclide('Pu239', 0.036623)
-mat.add_nuclide('Pu240', 0.00066951)
-mat.add_nuclide('Ga69', 0.0013211)
-mat.add_nuclide('Ga71', 0.00087679)
-mat.add_nuclide('Fe54', 8.3274e-06)
-mat.add_nuclide('Fe56', 0.00013072)
-mat.add_nuclide('Fe57', 3.0189e-06)
-mat.add_nuclide('Fe58', 4.0177e-07)
-mat.add_element('C', 0.00029311)
-mat.add_nuclide('Ni58', 0.0012679)
-mat.add_nuclide('Ni60', 0.00048838)
-mat.add_nuclide('Ni61', 2.1229e-05)
-mat.add_nuclide('Ni62', 6.7689e-05)
-mat.add_nuclide('Ni64', 1.7238e-05)
-
-mats.append(mat)
-
-mats.export_to_xml()
+materials = openmc.Materials([pu])
+materials.export_to_xml()
